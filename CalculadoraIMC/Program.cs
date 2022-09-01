4b7c91c0069c4653f5835d5428b938ee9e2438e8
Kae <80605454+kaelucas123@users.noreply.github.com>
2022-08-31 22:34:56 -0300

    Add files via upload

---
+++ CalculadoraIMC/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CalculadoraIMC.Entities;
+
+namespace CalculadoraIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Qual seu peso? ");
+            double peso = double.Parse(Console.ReadLine());
+            Console.Write("Qual sua altura? ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+            string retorno = "none";
+            if (imc <= 18.4)
+            {
+                retorno = "Abaixo do peso";
+            }
+            if (18.5 <= imc && imc <= 24.9)
+            {
+                retorno = "Peso normal";
+            }
+            if (25 <= imc && imc <= 29.9)
+            {
+                retorno = "Sobrepeso";
+            }
+            if (30 <= imc && imc <= 34.9)
+            {
+                retorno = "Obesidade grau I";
+            }
+            if (35 <= imc && imc <= 39.9)
+            {
+                retorno = "Obesidade grau II";
+            }
+            if (imc >= 40)
+            {
+                retorno = "Obesidade grau III";
+            }
+
+            Console.WriteLine($"Seu IMC é de = {imc.ToString("F2")} Sua situação: {retorno}"); 
+        }
+    }
+}
